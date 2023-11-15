1e1d1a6d5cabb7adc87267b9d0cc360c70fbf027
Allene Myria <allenemy@at-i6.msk.21-school.ru>
2023-11-15 18:30:33 +0300

    flags and make

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -7,9 +7,10 @@
 void open_file(char *template, char *name, char *flags, int *file_count);
 void add_letters(char *flags, char flag);
 void parser(int argc, char *argv[], char *flags, char *template, int *end_flags);
-void change_file(char *flags, FILE *fp, regex_t reg, char *name, int *file_count);
+void change_file(char *flags, FILE *fp, regex_t reg, char *name, int *file_count, char *template);
 void e_action(int *e_count, int *i, int *end_flags, char *template, char *argv[]);
-//void find_files(int argc, char *argv[], int *index);
+void f_action(int *i, int *end_flags, char *template, char *argv[]);
+//void mult_e_action(int *e_count, int *i, int *j, int *end_flags, char *template, char *argv[]);
 
 
 int main(int argc, char *argv[])
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
         char flags[10] = "";
         int end_flags = 1;
         int file_count = 1;
-        if (argc < 3) fprintf(stderr, "grep: Введите шаблон и/или имя файла\n");
+        if (argc < 3) fprintf(stderr, "grep: option requires an argument -- e\n");
         if (argc == 3) 
         {
             sprintf(template, "%s", argv[1]);
@@ -34,7 +35,6 @@ int main(int argc, char *argv[])
                 open_file(template, argv[i], flags, &file_count);
             }
        }
-    
     return 0;
 }
 
@@ -43,7 +43,7 @@ void parser(int argc, char *argv[], char *flags, char *template, int *end_flags)
     int e_count = 0;
     for (int i = 1; i < argc; i++)
     {   
-        if ((argv[i][0] == '-') && (argv[i][1] != 'e') && (strlen(argv[i]) == 2)) {
+        if ((argv[i][0] == '-') && (argv[i][1] != 'e') && (strlen(argv[i]) == 2) && (argv[i][1] != 'f')) {
             add_letters(flags, argv[i][1]);
             if (argv[i+1][0] != '-')
             { 
@@ -57,19 +57,13 @@ void parser(int argc, char *argv[], char *flags, char *template, int *end_flags)
             e_count += 1;
             add_letters(flags, argv[i][1]);
             e_action(&e_count, &i, end_flags, template, argv);
-            /*if (e_count == 1) 
-            {
-                sprintf(template, "%s", argv[i+1]);
-                *end_flags = i + 1;
-            }
-            else 
-            {
-                strcat(template, "|");
-                strcat(template, argv[i + 1]);
-                *end_flags = i + 1;
-            }*/
             if (argv[i + 2][0] != '-') break;
-        }  
+        }
+        if ((argv[i][0] == '-') && (argv[i][1] == 'f') && (strlen(argv[i]) == 2)) 
+        {
+            add_letters(flags, argv[i][1]);
+            f_action( &i, end_flags, template, argv);
+        }
         if (argv[1][0] != '-')
         {
             sprintf(template, "%s", argv[1]);
@@ -79,22 +73,53 @@ void parser(int argc, char *argv[], char *flags, char *template, int *end_flags)
         if ((argv[i][0] == '-') && (strlen(argv[i]) > 2))
         {
             for (int j = 1; j < (int)strlen(argv[i]); j++) {
-                
-                add_letters(flags, argv[i][j]);
-            }
-                if ((strchr(flags, 'e') != NULL) && (argv[i+1][0] != '-'))
-                { 
-                    e_count += 1;
-                    e_action(&e_count, &i, end_flags, template, argv);
-                if (strchr(flags, 'e') == NULL)  
+                /*if ((argv[i][j] != 'e') && (argv[i][j] != 'f')) add_letters(flags, argv[i][j]);
+                if (argv[i][j] == 'e')
                 {
-                    sprintf(template, "%s", argv[i+1]);
-                    *end_flags = i + 1;
-                    //break;
-                }  
-            } 
+                    add_letters(flags, argv[i][j]);
+                    e_count += 1;
+                    mult_e_action(&e_count, &i, &j,end_flags, template, argv);
+                }
+            }*/
+            add_letters(flags, argv[i][j]);
+            }
+            if ((strchr(flags, 'e') != NULL) && (argv[i+1][0] != '-'))
+            { 
+                e_count += 1;
+                e_action(&e_count, &i, end_flags, template, argv);
+            }
+            if ((strchr(flags, 'e') == NULL)  && (strchr(flags, 'f') == NULL))
+            {
+                sprintf(template, "%s", argv[i+1]);
+                *end_flags = i + 1;
+            }
+            if ((strchr(flags, 'f') != NULL) && (argv[i+1][0] != '-'))
+            {
+                f_action( &i, end_flags, template, argv);
+            }
         }
     }
+}
+void f_action(int *i, int *end_flags, char *template, char *argv[])
+{
+    char text[LEN] = "";
+    FILE *fp = fopen(argv[*i + 1], "r");
+    if (fp != NULL)
+    {
+        fgets(text, LEN, fp);
+        text[strlen(text) - 1] = '\0';
+        sprintf(template, "%s", text);
+
+        while (fgets(text, LEN, fp) != NULL)
+        {
+            text[strlen(text) - 1] = '\0';
+            strcat(template, "|");
+            strcat(template, text);
+        }
+    *end_flags = *i + 1;    
+    }
+    else fprintf(stderr, "grep: %s: No such file or directory\n", argv[*i + 1]);
+
 }
 
 void e_action(int *e_count, int *i, int *end_flags, char *template, char *argv[])
@@ -111,6 +136,27 @@ void e_action(int *e_count, int *i, int *end_flags, char *template, char *argv[]
         *end_flags = *i + 1;
     }
 }
+/*void mult_e_action(int *e_count, int *i, int *j, int *end_flags, char *template, char *argv[])
+{
+    if (*e_count == 1) 
+    {
+        //for (int m = *j + 1; m < (int)strlen(argv[*i]); *j = *j + 1)
+        //{
+            strcat(template, argv[*i][*(j + 1)]);
+        ///}
+        
+        *end_flags = *i + 1;
+    }
+    else 
+    {
+        strcat(template, "|");
+        //for (int m = *j + 1; m < (int)strlen(argv[*i]); *j = *j + 1)
+        //{
+            strcat(template, argv[*i][*(j + 1)]);
+        //}
+        *end_flags = *i + 1;
+    }
+}*/
 
 
 void add_letters(char *flags, char flag)
@@ -127,15 +173,17 @@ void open_file(char *template, char *name, char *flags, int *file_count){
     
     regex_t reg;
     FILE *fp = fopen(name, "r");
+    if (fp != NULL){
     if (strchr(flags, 'i') != NULL) regcomp(&reg, template, REG_ICASE | REG_EXTENDED);
     else regcomp(&reg, template, REG_EXTENDED);
-    change_file(flags, fp, reg, name, file_count);
+    change_file(flags, fp, reg, name, file_count, template);
     regfree(&reg);
+    }
+    if ((fp == NULL) && (strchr(flags, 's') == NULL)) fprintf(stderr, "grep: %s: No such file or directory\n", name);
     fclose(fp); 
-
 }
 
-void change_file(char *flags, FILE *fp, regex_t reg, char *name, int *file_count)
+void change_file(char *flags, FILE *fp, regex_t reg, char *name, int *file_count, char *template)
 {
     //printf("%d\n", *file_count);
     char txt[LEN] = "";
@@ -147,14 +195,30 @@ void change_file(char *flags, FILE *fp, regex_t reg, char *name, int *file_count
         int flag = 0;
         if (status == REG_NOMATCH && (strchr(flags, 'v') != NULL)) flag = 1;
         if (status == 0 && (strchr(flags, 'v') == NULL)) flag = 1;
-        
-        if ((*file_count > 1) && (flag== 1) && (strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (strchr(flags, 'n') == NULL)) printf("%s:%s", name, txt);
-        if ((*file_count > 1) && (flag== 1) && (strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (strchr(flags, 'n') != NULL)) printf("%s:%d:%s", name, cur_line, txt);
+        if ((*file_count > 1) && (flag== 1)) 
+        {
+            if ((strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (strchr(flags, 'n') == NULL) && (strchr(flags, 'h') == NULL)) 
+                {
+                    if (strchr(flags, 'o') == NULL) printf("%s:%s", name, txt);
+                    else printf("%s:%s\n", name, template);
+                }
+            if ((strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (strchr(flags, 'n') != NULL)) 
+            {
+                if (strchr(flags, 'o') == NULL) printf("%s:%d:%s", name, cur_line, txt);
+                else printf("%s:%d:%s\n", name, cur_line, template);
+            }
+            if ((strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (strchr(flags, 'n') == NULL) && (strchr(flags, 'h') != NULL)) 
+            {
+                if (strchr(flags, 'o') == NULL) printf("%s",txt);
+                else  printf("%s\n",template);
+            }
+        }
         if ((*file_count == 1) && (flag == 1) && (strchr(flags, 'n') != NULL) && (strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL)) printf("%d:", cur_line);
-        if ((flag== 1) && (strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (*file_count == 1)) printf("%s", txt);
-       
-        
-        
+        if ((flag == 1) && (strchr(flags, 'c') == NULL)  && (strchr(flags, 'l') == NULL) && (*file_count == 1)) 
+        {
+            if (strchr(flags, 'o') == NULL) printf("%s", txt);
+            else  printf("%s\n",template);
+        }
         line_matches += flag;
         cur_line++;
         }
--- src/grep/test.sh
+++ src/grep/test.sh
@@ -8,17 +8,24 @@ DIFF_RES=""
 declare -a tests=(
    "ghj test.txt"
    "VAR ghj test.txt"
-   "VAR ghj test.txt test.txt"
+   "VAR ghj test.txt txt.txt"
 )
 
 declare -a tests1=(
-    "-e"
+    "VARe"
+    "VARe ghj -e vbm test.txt"
+    "VARe ghj -e vbm test.txt txt.txt"
+    "VARne ghj test.txt"
+    "VARe ghj nofile"
+    ""^t" test.txt"
+    "VARiv ghj test.txt"
+    "VARf -n test_copy.txt test.txt"
 )
 
 get_test()
 {
     templ=$(echo $@ | sed "s/VAR/$var/")
-    ./grep $templ >> test_s21_grep.log
+    ./s21_grep $templ >> test_s21_grep.log
     grep $templ >> test_grep.log
     DIFF_RES="$(diff -s test_s21_grep.log test_grep.log)"
         (( COUNTER++ ))
@@ -33,7 +40,7 @@ get_test()
 rm test_s21_grep.log test_grep.log
 }
 
-for var1 in n c l e i v
+for var1 in h n c l e i v s o f
 do
     for i in "${tests[@]}"
     do
